fc953a58b52bef20de4dc406342bde4929b7e963
Leon <silenceace@gmail.com>
2024-11-29 18:29:51 +0800

    fix: update entrypoint script to clarify configuration file path

--- Docker/nginx/entrypoint.sh
+++ Docker/nginx/entrypoint.sh
@@ -17,6 +17,6 @@ echo -e "${GREEN}$(nginx -v 2>&1)${NC}"
 echo -e "\n${YELLOW}Installed extra modules:${NC}"
 echo -e "${BLUE}ngx_http_geoip_module${NC}, ${BLUE}ngx_http_image_filter_module${NC}, ${BLUE}ngx_http_perl_module${NC}, ${BLUE}ngx_http_xslt_filter_module${NC}, ${BLUE}ngx_mail_module${NC}, ${BLUE}ngx_stream_geoip_module${NC}, ${BLUE}ngx_stream_module${NC}, ${BLUE}ngx-fancyindex${NC}, ${BLUE}headers-more-nginx-module${NC}, etc."
 echo -e "\n${YELLOW}nginx.conf configuration file path:${NC} ${RED}/etc/nginx/nginx.conf${NC}"
-echo -e "${YELLOW}conf.d configuration file path:${NC} ${RED}/etc/nginx/conf.d${NC}"
+echo -e "${YELLOW}server configuration file path:${NC} ${RED}/etc/nginx/conf.d${NC}"
 
 nginx -g "daemon off;"
