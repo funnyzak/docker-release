9dc9e7ef0fd90ed79fa5d62521e9bc8e4f894bbe
Leon <silenceace@gmail.com>
2024-12-01 11:21:08 +0800

    feat: update MySQL dump configuration and enhance entrypoint script with color-coded output

--- Docker/mysql-dump/scripts/entrypoint.sh
+++ Docker/mysql-dump/scripts/entrypoint.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
 set -e
 
-echo -e "mysql-dump is a simple MySQL backup tool that can backup all databases or specified databases. It can push message with pushoo and delete expired dump files. It supports custom commands before and after the dump, custom mysqldump options, compressed dump files, and crontab rules."
-echo -e "Docker Hub: https://hub.docker.com/r/funnyzak/mysql-dump"
-echo -e "GitHub: https://github.com/funnyzak/docker-release\n"
+echo -e "${BLUE}mysql-dump is a simple MySQL backup tool that can backup all databases or specified databases. It can push message with pushoo and delete expired dump files. It supports custom commands before and after the dump, custom mysqldump options, compressed dump files, and crontab rules.${NC}\n"
+
+echo -e "${GREEN}Docker Hub: https://hub.docker.com/r/funnyzak/mysql-dump${NC}"
+echo -e "${GREEN}GitHub: https://github.com/funnyzak/docker-release\n"
 
 chmod +x /run-scripts/*
 
@@ -15,12 +22,12 @@ echo -e "$CRON_STRINGS\n" > /var/spool/cron/crontabs/CRON_STRINGS
 chmod -R 0644 /var/spool/cron/crontabs
 
 if [ -n "$STARTUP_COMMAND" ]; then
-    echo -e "Execute startup command: $STARTUP_COMMAND"
-    $STARTUP_COMMAND 2>tmp_error_log || (echo "execute after dump command failed. error message: `cat tmp_error_log`")
+  echo -e "\n${YELLOW}Executing Start Up commands: ${NC}$STARTUP_COMMAND"
+  (eval "$STARTUP_COMMAND") && echo -e "${GREEN}Start Up commands executed successfully.${NC}\n" || (echo -e "${RED}Failed to execute Start Up commands: $STARTUP_COMMAND${NC}\n")
 fi
 
 if [ -n "$DUMP_AT_STARTUP" -a "$DUMP_AT_STARTUP" = "true" ]; then
-    /run-scripts/cron-backup.sh >> /var/log/cron/cron.log 2>&1 &
+  /run-scripts/cron-backup.sh >> /var/log/cron/cron.log 2>&1 &
 fi
 
 crond -s /var/spool/cron/crontabs -b -L /var/log/cron/cron.log "$@" && tail -f /var/log/cron/cron.log
