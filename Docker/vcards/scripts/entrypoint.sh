69e3b91a2ea98bb4858f2703c0a27adbe5fab9fa
Leon <silenceace@gmail.com>
2024-12-19 14:56:45 +0800

    chore: update repository name in Dockerfiles and scripts

--- Docker/vcards/scripts/entrypoint.sh
+++ Docker/vcards/scripts/entrypoint.sh
@@ -8,11 +8,11 @@ YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
-echo -e "${GREEN}Metowolf/vCards $(cat /app/VERSION)${NC}"
+echo -e "${GREEN}${REPO_NAME} $(cat /app/VERSION)${NC}"
 echo -e "${BLUE}Import frequently used contact avatars and optimize iOS caller and message interface experience.${NC}\n"
 
 echo -e "${GREEN}Docker Hub: https://hub.docker.com/r/funnyzak/vcards${NC}"
-echo -e "${GREEN}Repository: https://github.com/metowolf/vCards${NC}"
+echo -e "${GREEN}Repository: https://github.com/${REPO_NAME}${NC}"
 echo -e "${GREEN}Build via: https://github.com/funnyzak/docker-release${NC}\n"
 
 echo -e "${GREEN}Has vCards: $(find /app/vcards/collection-root/cn -name '*.vcf' | wc -l) files.${NC}\n"
@@ -27,7 +27,7 @@ if [ -n "$SYNC_CRON" ]; then
   echo -e "${GREEN}Radicale is running.${NC}"
 
   echo -e "\n${GREEN}The vCard will be synchronized according to the following cron expression: ${SYNC_CRON}${NC}"
-  echo -e "${YELLOW}Note: fetch the latest vCards from the repo: https://github.com/metowolf/vCards${NC}\n"
+  echo -e "${YELLOW}Note: fetch the latest vCards from the repo: https://github.com/${REPO_NAME}${NC}\n"
 
   CRON_STRINGS="$SYNC_CRON /run-scripts/cron-vcard.sh >> /var/log/cron/cron.log 2>&1"
   echo -e "$CRON_STRINGS\n" > /var/spool/cron/crontabs/root
