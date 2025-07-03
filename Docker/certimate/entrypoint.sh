8b2188a1b49c4606e95d0dd182ede34a640a92ae
Leon <silenceace@gmail.com>
2025-07-03 11:39:45 +0800

    Update Dockerfile and repository references for certimate-go organization (#38)
    * Update Dockerfile to use golang:1.24.4-alpine and optimize package installation
    
    - Updated base image from golang:1.24.1-alpine to golang:1.24.4-alpine for improved stability.
    - Changed package installation to use --no-cache for better efficiency and reduced image size.
    - Added comment to clarify the necessity of git installation for the application.
    
    * Update repository references in Docker and workflow files to point to certimate-go organization
    
    - Changed GitHub repository URLs from usual2970 to certimate-go in the Dockerfile, entrypoint script, README, and GitHub Actions workflow.
    - Updated the command for fetching the official version in the workflow to reflect the new repository location.
    
    * chore:update

--- Docker/certimate/entrypoint.sh
+++ Docker/certimate/entrypoint.sh
@@ -12,7 +12,7 @@ echo -e "${GREEN}Certimate $(cat /app/VERSION)${NC}"
 echo -e "${BLUE}Certimate aims to provide users with a secure and user-friendly SSL certificate management solution.${NC}\n"
 
 echo -e "${GREEN}Docker Hub: https://hub.docker.com/r/funnyzak/certmate${NC}"
-echo -e "${GREEN}Repository: https://github.com/usual2970/certimate${NC}"
+echo -e "${GREEN}Repository: https://github.com/certimate-go/certimate${NC}"
 echo -e "${GREEN}Build via: https://github.com/funnyzak/docker-release${NC}\n"
 
 /app/certimate serve --http 0.0.0.0:${PORT:-8090}
